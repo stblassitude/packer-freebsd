4e24cdee9097b2cde03512021e53e83cc1fe9ebb
Stefan Bethke <stb@lassitu.de>
2016-11-29 23:48:26 +0100

    Add timeout to dd command in case it get's stuck

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-set -ex
+set -x
 
 if zfs list -H -o name zroot; then
   zfs create -o compression=off -o sync=standard -o mountpoint=/var/tmp zroot/empty
 fi
-dd if=/dev/zero of=/var/tmp/EMPTY bs=1M || :
+
+# it appears dd can hang when the dataset is full, so run it with a timeout
+timeout 15m dd if=/dev/zero of=/var/tmp/EMPTY bs=1M || :
+
 rm -f /var/tmp/EMPTY
-zfs destroy zroot/empty || :
+zfs destroy zroot/empty
 sync
 sleep 30
 sync
