3e571be41c93947abd1a1868517e591a1248fdf8
Stefan Bethke <stb@lassitu.de>
2016-11-29 23:00:31 +0100

    Move removal of empty dataset before the sleep/sync rounds

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -3,10 +3,10 @@ set -ex
 
 if zfs list -H -o name zroot; then
   zfs create -o compression=off -o sync=standard -o mountpoint=/var/tmp zroot/empty
-  trap 'zfs destroy zroot/empty' 0
 fi
 dd if=/dev/zero of=/var/tmp/EMPTY bs=1M || :
-rm /var/tmp/EMPTY
+rm -f /var/tmp/EMPTY
+zfs destroy zroot/empty || :
 sync
 sleep 30
 sync
